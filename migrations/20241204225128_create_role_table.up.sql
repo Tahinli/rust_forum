c6d3246f711d0dab7cd19f9f3373c5faa4b8c5f9
Ahmet Kaan GÜMÜŞ <96421894+Tahinli@users.noreply.github.com>
2024-12-08 06:05:25 +0300

    feat: :sparkles: new role maintainer

--- migrations/20241204225128_create_role_table.up.sql
+++ migrations/20241204225128_create_role_table.up.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS "role"(
 
 INSERT INTO "role"(id, name) VALUES (0, 'Ahmet Kaan Gümüş') ON CONFLICT(id) DO UPDATE SET "name" = 'Ahmet Kaan Gümüş';
 INSERT INTO "role"(id, name) VALUES (1, 'Founder') ON CONFLICT(id) DO UPDATE SET "name" = 'Founder';
-INSERT INTO "role"(id, name) VALUES (2, 'Admin') ON CONFLICT(id) DO UPDATE SET "name" = 'Admin';
+INSERT INTO "role"(id, name) VALUES (2, 'Maintainer') ON CONFLICT(id) DO UPDATE SET "name" = 'Founder';
+INSERT INTO "role"(id, name) VALUES (3, 'Admin') ON CONFLICT(id) DO UPDATE SET "name" = 'Admin';
 INSERT INTO "role"(id, name) VALUES (10, 'Normal') ON CONFLICT(id) DO UPDATE SET "name" = 'Normal';
 INSERT INTO "role"(id, name) VALUES (-1, 'Banned') ON CONFLICT(id) DO UPDATE SET "name" = 'Banned';
 
@@ -48,11 +49,18 @@ ON CONFLICT(role_id) DO UPDATE SET
 "create_lower" = TRUE, "read_lower" = TRUE, "update_lower" = TRUE, "delete_lower" = TRUE;
 
 INSERT INTO "permission_role"(role_id, create_self, read_self, update_self, delete_self, create_other, read_other, update_other, delete_other, create_lower, read_lower, update_lower, delete_lower) 
-VALUES (10, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE) 
+VALUES (3, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE) 
+ON CONFLICT(role_id) DO UPDATE SET 
+"create_self" = TRUE, "read_self" = TRUE, "update_self" = TRUE, "delete_self" = TRUE, 
+"create_other" = TRUE, "read_other" = TRUE, "update_other" = TRUE, "delete_other" = TRUE, 
+"create_lower" = TRUE, "read_lower" = TRUE, "update_lower" = TRUE, "delete_lower" = TRUE;
+
+INSERT INTO "permission_role"(role_id, create_self, read_self, update_self, delete_self, create_other, read_other, update_other, delete_other, create_lower, read_lower, update_lower, delete_lower) 
+VALUES (10, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE) 
 ON CONFLICT(role_id) DO UPDATE SET 
 "create_self" = FALSE, "read_self" = TRUE, "update_self" = FALSE, "delete_self" = FALSE, 
-"create_other" = FALSE, "read_other" = FALSE, "update_other" = FALSE, "delete_other" = FALSE, 
-"create_lower" = FALSE, "read_lower" = FALSE, "update_lower" = FALSE, "delete_lower" = FALSE;
+"create_other" = FALSE, "read_other" = TRUE, "update_other" = FALSE, "delete_other" = FALSE, 
+"create_lower" = FALSE, "read_lower" = TRUE, "update_lower" = FALSE, "delete_lower" = FALSE;
 
 INSERT INTO "permission_role"(role_id, create_self, read_self, update_self, delete_self, create_other, read_other, update_other, delete_other, create_lower, read_lower, update_lower, delete_lower) 
 VALUES (-1, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE) 
--- migrations/20241204225143_create_post_table.up.sql
+++ migrations/20241204225143_create_post_table.up.sql
@@ -35,7 +35,7 @@ ON CONFLICT(role_id) DO UPDATE SET
 "create_other" = TRUE, "read_other" = TRUE, "update_other" = TRUE, "delete_other" = TRUE, 
 "create_lower" = TRUE, "read_lower" = TRUE, "update_lower" = TRUE, "delete_lower" = TRUE;
 
-INSERT INTO "permission_post"(role_id, create_self, read_self, update_self, delete_self, create_other, read_other, update_other, delete_other, create_lower, read_lower, update_lower, delete_lower) 
+INSERT INTO "permission_role"(role_id, create_self, read_self, update_self, delete_self, create_other, read_other, update_other, delete_other, create_lower, read_lower, update_lower, delete_lower) 
 VALUES (2, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE) 
 ON CONFLICT(role_id) DO UPDATE SET 
 "create_self" = TRUE, "read_self" = TRUE, "update_self" = TRUE, "delete_self" = TRUE, 
@@ -43,11 +43,18 @@ ON CONFLICT(role_id) DO UPDATE SET
 "create_lower" = TRUE, "read_lower" = TRUE, "update_lower" = TRUE, "delete_lower" = TRUE;
 
 INSERT INTO "permission_post"(role_id, create_self, read_self, update_self, delete_self, create_other, read_other, update_other, delete_other, create_lower, read_lower, update_lower, delete_lower) 
-VALUES (10, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE) 
+VALUES (3, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE) 
+ON CONFLICT(role_id) DO UPDATE SET 
+"create_self" = TRUE, "read_self" = TRUE, "update_self" = TRUE, "delete_self" = TRUE, 
+"create_other" = TRUE, "read_other" = TRUE, "update_other" = TRUE, "delete_other" = TRUE, 
+"create_lower" = TRUE, "read_lower" = TRUE, "update_lower" = TRUE, "delete_lower" = TRUE;
+
+INSERT INTO "permission_post"(role_id, create_self, read_self, update_self, delete_self, create_other, read_other, update_other, delete_other, create_lower, read_lower, update_lower, delete_lower) 
+VALUES (10, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE) 
 ON CONFLICT(role_id) DO UPDATE SET 
 "create_self" = FALSE, "read_self" = TRUE, "update_self" = FALSE, "delete_self" = FALSE, 
-"create_other" = FALSE, "read_other" = FALSE, "update_other" = FALSE, "delete_other" = FALSE, 
-"create_lower" = FALSE, "read_lower" = FALSE, "update_lower" = FALSE, "delete_lower" = FALSE;
+"create_other" = FALSE, "read_other" = TRUE, "update_other" = FALSE, "delete_other" = FALSE, 
+"create_lower" = FALSE, "read_lower" = TRUE, "update_lower" = FALSE, "delete_lower" = FALSE;
 
 INSERT INTO "permission_post"(role_id, create_self, read_self, update_self, delete_self, create_other, read_other, update_other, delete_other, create_lower, read_lower, update_lower, delete_lower) 
 VALUES (-1, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE) 
